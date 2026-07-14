2b10cf8aa2bf1c930ee9f9133ae1fecfd17a1991
NikWork <nik8021@gmail.com>
2026-07-14 08:39:34 +0300

    fix: type_of_data.html and not decided task5

--- 5.Типы данных/5task.js
+++ 5.Типы данных/5task.js
@@ -1,35 +1,19 @@
-arr = [-2, -1, 1, 2]
+arr = [2, -1, 2, 3, -9]
 function getMaxSubSum(arr) {
-    let maxSum = {
-        sum: 0,
-        begin: -1,
-        end: -1
-    }
-    let sum = 0;
-    let begin = 0;
-    let end = 0;
+    let maxSum = [0]
     for (let i = 0; i < arr.length; i++) {
-        if (i == 0 && arr[i] > sum) {
-            sum += arr[i];
-            begin = i;
-        } else if (arr[i] > arr[i - 1] && i < (arr.length-1)) {
-            sum += arr[i];
-            end = i;
-        } else if (arr[i] < arr[i - 1]) {
-            maxSum.sum = sum;
-            sum = 0;
-            maxSum.begin = begin;
-            begin = 0;
-            maxSum.end = end;
-            end = 0;
+        let tempSum = maxSum[0]
+        for (let j = i + 1; j < arr.length; j++) {
+
+            if ((tempSum + arr[j]) > maxSum[0]) {
+
+                tempSum += arr[j]
+                if (tempSum > maxSum[0]) maxSum[0] = tempSum
+            }
         }
-        if (sum == 0 && i == (arr.length - 1) && arr[i] > maxSum.sum) {
-            maxSum.sum = arr[i];
-            maxSum.begin = i;
-            maxSum.end = i;
-        }
+        if (arr[i] > maxSum[0]) maxSum[0] = arr[i];
     }
-    return maxSum.sum
+    return maxSum[0]
 }
 
 console.log(getMaxSubSum(arr))
